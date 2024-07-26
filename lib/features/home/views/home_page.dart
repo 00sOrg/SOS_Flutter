9c01d33a856a3907945322d11d487e6a9a412885
cherrie-k <cherriekim925@gmail.com>
2024-07-26 19:13:58 +0900

    [Feat] 홈 화면에 지도 띄움

--- lib/features/home/views/home_page.dart
+++ lib/features/home/views/home_page.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sos/features/home/views/widgets/map_area.dart';
+import 'package:sos/shared/providers/location_provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locationAsyncValue = ref.watch(locationProvider);
+
     return Scaffold(
-      body: Center(child: Text('Home Page'),),
+      body: Stack(
+        children: [
+          locationAsyncValue.when(
+            data: (location) {
+              return MapArea(location: location);
+            },
+            error: (error, stack) => Center(
+              child: Text(
+                'Error: $error',
+              ),
+            ),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/features/home/views/widgets/map_area.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:sos/shared/models/location.dart';
+
+class MapArea extends StatelessWidget {
+  final Location location;
+
+  const MapArea({super.key, required this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return NaverMap(
+      options: NaverMapViewOptions(
+        initialCameraPosition: NCameraPosition(
+          target: NLatLng(location.latitude, location.longitude),
+          zoom: 15,
+          bearing: 0,
+          tilt: 0,
+        ),
+      ),
+      onMapReady: (controller) {
+        debugPrint('[[SUCCESS]] 네이버 지도 로딩 \n');
+      },
+    );
+  }
+}
