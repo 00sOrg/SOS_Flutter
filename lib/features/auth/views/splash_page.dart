10ccbb76c00e656ae33cb07f04d12d05d6c8f0d7
cherrie-k <cherriekim925@gmail.com>
2024-08-02 03:20:19 +0900

    [Feat] apply ScreenUtil for responsive UI

---
+++ lib/features/auth/views/splash_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/icons/logo.svg',
+          width: 108.w,
+        ),
+      ),
+    );
+  }
+}
