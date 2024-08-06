37836c2561fa07c477574e66a4cb9e2793d3400f
cherrie-k <cherriekim925@gmail.com>
2024-08-07 02:59:41 +0900

    [Feat] add Write Page

---
+++ lib/features/write/viewmodels/write_viewmodel.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:sos/shared/models/post.dart';
+import 'package:sos/shared/repositories/post_repository.dart';
+
+class WriteViewModel extends StateNotifier<Post> {
+  final PostRepository _repository;
+
+  WriteViewModel(this._repository)
+      : super(
+          Post(
+            id: '',
+            title: '',
+            address: '',
+            category: '',
+            image: null,
+            content: '',
+            createdAt: DateTime.now(),
+          ),
+        );
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> createPost(String address) async {
+    if (state.image != null) {
+      final imageUrl = await _repository.uploadImage(File(state.image!));
+      state = state.copyWith(image: imageUrl);
+  }
+    state = state.copyWith(address: address);  // address는 직접 입력 받는게 아니라 따로 추가해줌
+    await _repository.createPost(state);
+  }
+
+  void updateTitle(String title) {
+    state = state.copyWith(title: title);
+  }
+
+  void updateAddress(String address) {
+    state = state.copyWith(address: address);
+  }
+
+  void updateCategory(String category) {
+    state = state.copyWith(category: category);
+  }
+
+  void updateContent(String content) {
+    state = state.copyWith(content: content);
+  }
+
+  Future<void> pickImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      state = state.copyWith(image: image.path);
+    }
+  }
+}
+
+final writeViewModelProvider = StateNotifierProvider<WriteViewModel, Post>(
+  (ref) {
+    final repository = ref.watch(postRepositoryProvider);
+    return WriteViewModel(repository);
+  },
+);
--- lib/features/write/views/write_page.dart
+++ lib/features/write/views/write_page.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:keyboard_dismisser/keyboard_dismisser.dart';
 
-class PostPage extends StatelessWidget {
-  const PostPage({super.key});
+class WritePage extends ConsumerWidget {
+  const WritePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Write Page'),),
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final viewModel = ref.watch(writeViewModelProvider.notifier);
+
+    return KeyboardDismisser(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFFFFFFF),
+          title: const Text(
+            '사건/사고 게시물 작성',
+            style: TextStyle(fontSize: 17),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Divider(height: 1, thickness: 1),
+                const SizedBox(height: 21),
+                titleField(),
+                const SizedBox(height: 9),
+                Container(
+                  height: 1.3,
+                  color: const Color(0xFFE9E9E9),
+                ),
+                const SizedBox(height: 12),
+                contentField(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget titleField() {
+    return TextField(
+      keyboardType: TextInputType.text,
+      autocorrect: false,
+      maxLines: 1,
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        hintText: '제목',
+        hintStyle: TextStyle(
+          fontSize: 16,
+          color: Color(0xFFB6B6B6),
+          fontWeight: FontWeight.w700,
+        ),
+        border: InputBorder.none,
+      ),
+      style: const TextStyle(
+        color: Color(0xFF060606),
+        fontWeight: FontWeight.w700,
+      ),
+      onTap: () {},
+    );
+  }
+
+  Widget contentField() {
+    return TextField(
+      keyboardType: TextInputType.multiline,
+      autocorrect: false,
+      maxLines: null,
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        hintText: '내용을 입력하세요',
+        hintStyle: TextStyle(
+          fontSize: 14,
+          color: Color(0xFFB6B6B6),
+          fontWeight: FontWeight.w400,
+        ),
+        border: InputBorder.none,
+      ),
+      style: const TextStyle(
+        fontSize: 14,
+        color: Color(0xFF060606),
+        fontWeight: FontWeight.w400,
+      ),
+      onTap: () {},
     );
   }
 }
