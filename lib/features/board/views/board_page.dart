43089fd458f8500705cd811bc84f8cf9a987cc33
cherrie-k <cherriekim925@gmail.com>
2024-08-09 17:36:32 +0900

    [Feat] add grid layout for Board Page

--- lib/features/board/views/board_page.dart
+++ lib/features/board/views/board_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 class BoardPage extends StatelessWidget {
-  const BoardPage({super.key});
+  BoardPage({super.key});
+
+  final List<String> dummyPics =
+      List.generate(20, (index) => 'https://picsum.photos/20${index % 10}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 1,
+                  mainAxisSpacing: 1,
+                  childAspectRatio: 1,
+                ),
+                itemCount: dummyPics.length,
+                itemBuilder: (context, idx) {
+                  return Container(
+                    child: Image.network(
+                      dummyPics[idx],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
