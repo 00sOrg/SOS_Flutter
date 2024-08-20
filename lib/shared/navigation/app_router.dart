082bc68a5d5b9097f4259e61511cdb04327e3a81
cherrie-k <cherriekim925@gmail.com>
2024-08-21 06:22:53 +0900

    [Feat] set initialLocation to default path

--- lib/shared/navigation/app_router.dart
+++ lib/shared/navigation/app_router.dart
@@ -24,7 +24,7 @@ class AppRouter {
 
   GoRouter router(String locationAddress) {
     return GoRouter(
-      initialLocation: '/location-test',
+      initialLocation: '/',
       routes: [
         GoRoute(
           path: '/',
