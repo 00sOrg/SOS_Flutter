c0f2640200f8aa66b35a539cd7d0183077b22ae5
cherrie-k <cherriekim925@gmail.com>
2024-09-16 23:05:48 +0900

    [Feat] add new method for location viewmodel

--- lib/shared/services/location_service.dart
+++ lib/shared/services/location_service.dart
@@ -1,19 +1,20 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class LocationService {
   Future<Map<String, dynamic>> fetchLocationData(double lat, double lon) async {
+    final String baseGeoUrl = dotenv.env['NAVER_REVERSE_GEOCODE_URL']!;
+    final String fullUrl =
+        '$baseGeoUrl?coords=$lon,$lat&sourcecrs=epsg:4326&orders=legalcode,admcode,addr,roadaddr&output=json';
+
     final headers = {
       'X-NCP-APIGW-API-KEY-ID': dotenv.env['NAVER_MAP_API_ID']!,
       'X-NCP-APIGW-API-KEY': dotenv.env['NAVER_MAP_API_SECRET']!,
     };
 
     final response = await http.get(
-      Uri.parse(
-        "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=$lon,$lat&sourcecrs=epsg:4326&orders=legalcode,admcode,addr,roadaddr&output=json",
-      ),
+      Uri.parse(fullUrl),
       headers: headers,
     );
 
