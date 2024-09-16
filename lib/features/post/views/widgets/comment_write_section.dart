456fb2aa36491b08e1383674183878303bda774f
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-09-17 01:21:39 +0900

    [Fix] Comment Write Section UI

--- lib/features/post/views/widgets/comment_write_section.dart
+++ lib/features/post/views/widgets/comment_write_section.dart
@@ -6,50 +6,46 @@ class CommentWriteSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      // Use Expanded to let the container grow with its content
-      child: Container(
-        color: AppColors.lineGray,
-        padding: const EdgeInsets.fromLTRB(16, 11, 13, 8),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Add a comment...',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    borderSide: BorderSide.none,
-                  ),
-                  filled: true,
-                  fillColor: AppColors.white,
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+    return Container(
+      color: AppColors.lineGray,
+      padding: const EdgeInsets.fromLTRB(16, 11, 13, 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Add a comment...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  borderSide: BorderSide.none,
                 ),
-                keyboardType: TextInputType.multiline,
-                minLines: 1,
-                maxLines: null,
+                filled: true,
+                fillColor: AppColors.white,
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
               ),
+              keyboardType: TextInputType.multiline,
+              minLines: 1,
+              maxLines: null,
             ),
-            const SizedBox(
-                width: 10), // Add space between the text field and the button
-            Container(
-              height: 41,
-              width: 41,
-              decoration: BoxDecoration(
-                color: AppColors.blue, // Replace with your desired color
-                shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.send, color: Colors.white),
-                onPressed: () {
-                  // TODO: 댓글 전송 로직 구현
-                  debugPrint('Send comment');
-                },
-              ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            height: 41,
+            width: 41,
+            decoration: BoxDecoration(
+              color: AppColors.blue,
+              shape: BoxShape.circle,
             ),
-          ],
-        ),
+            child: IconButton(
+              icon: const Icon(Icons.send, color: Colors.white),
+              onPressed: () {
+                // Implement your send comment logic
+                debugPrint('Send comment');
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
