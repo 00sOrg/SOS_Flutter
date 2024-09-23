cf8caec7e1f0f550e2096782c2b727445f219bcc
cherrie-k <cherriekim925@gmail.com>
2024-09-24 03:43:14 +0900

    [Design] update ui for Post page

--- lib/features/post/views/widgets/comment_write_section.dart
+++ lib/features/post/views/widgets/comment_write_section.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sos/features/post/viewmodels/post_viewmodel.dart';
 import 'package:sos/shared/styles/global_styles.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CommentWriteSection extends ConsumerStatefulWidget {
   final int postId; // Accept postId as a parameter
@@ -10,7 +9,8 @@ class CommentWriteSection extends ConsumerStatefulWidget {
   const CommentWriteSection({super.key, required this.postId});
 
   @override
-  _CommentWriteSectionState createState() => _CommentWriteSectionState();
+  ConsumerState<CommentWriteSection> createState() =>
+      _CommentWriteSectionState();
 }
 
 class _CommentWriteSectionState extends ConsumerState<CommentWriteSection> {
@@ -19,57 +19,70 @@ class _CommentWriteSectionState extends ConsumerState<CommentWriteSection> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppColors.white,
-      padding: const EdgeInsets.fromLTRB(16, 11, 13, 25),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                hintText: 'Add a comment...',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50),
-                  borderSide: BorderSide.none,
-                ),
-                filled: true,
-                fillColor: AppColors.lineGray,
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-              ),
-              keyboardType: TextInputType.multiline,
-              minLines: 1,
-              maxLines: null,
-            ),
-          ),
-          const SizedBox(width: 10),
-          Container(
-            height: 41,
-            width: 41,
-            decoration: BoxDecoration(
-              color: AppColors.blue,
-              shape: BoxShape.circle,
-            ),
-            child: IconButton(
-                icon: const Icon(Icons.send, color: AppColors.white),
-                onPressed: () {
-                  final comment = _controller.text;
-                  if (comment.isNotEmpty) {
-                    ref
-                        .read(postViewModelProvider(widget.postId).notifier)
-                        .addCommentToPost(widget.postId, comment)
-                        .then((_) {
-                      // Clear the comment field
-                      _controller.clear();
-                      // Refresh the post to show the new comment
-                      ref.refresh(postByIdProvider(widget.postId));
-                    });
-                  } else {
-                    debugPrint('Comment is empty');
-                  }
-                }),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 3),
+            color: AppColors.black.withOpacity(0.20),
+            blurRadius: 3,
+            spreadRadius: 2,
           ),
         ],
+      ),
+      padding: const EdgeInsets.fromLTRB(16, 14, 13, 14),
+      child: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: '댓글을 추가하세요',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: AppColors.lineGray,
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                ),
+                keyboardType: TextInputType.multiline,
+                minLines: 1,
+                maxLines: null,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Container(
+              height: 41,
+              width: 41,
+              decoration: const BoxDecoration(
+                color: AppColors.blue,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                  icon: const Icon(Icons.send, color: AppColors.white),
+                  onPressed: () {
+                    final comment = _controller.text;
+                    if (comment.isNotEmpty) {
+                      ref
+                          .read(postViewModelProvider(widget.postId).notifier)
+                          .addCommentToPost(widget.postId, comment)
+                          .then((_) {
+                        // Clear the comment field
+                        _controller.clear();
+                        // Refresh the post to show the new comment
+                        ref.refresh(postByIdProvider(widget.postId));
+                        FocusScope.of(context).unfocus();
+                      });
+                    } else {
+                      debugPrint('Comment is empty');
+                    }
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
