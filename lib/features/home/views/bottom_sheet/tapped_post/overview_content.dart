a6ce35312e0d0d86fae083d3f95dafdab8d20768
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-09-29 22:09:06 +0900

    [Feat] Post Overview Page Content

---
+++ lib/features/home/views/bottom_sheet/tapped_post/overview_content.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:sos/shared/styles/global_styles.dart';
+
+class OverviewContent extends StatelessWidget {
+  final String? content;
+
+  const OverviewContent({super.key, this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    if (content == null || content!.isEmpty) {
+      return const SizedBox.shrink(); // If empty, return an empty widget
+    }
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 30),
+      child: Container(
+        alignment: Alignment.topLeft,
+        child: SizedBox(
+          child: Text(
+            textAlign: TextAlign.justify,
+            content!,
+            style: const TextStyle(
+              color: AppColors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+              height: 1.2,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
