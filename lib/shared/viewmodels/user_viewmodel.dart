92d170f8b1d83b153f41892bb0e6b05bad8a9cd2
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-10-01 03:32:17 +0900

    [Fix] UserViewModel initializing method

--- lib/shared/viewmodels/user_viewmodel.dart
+++ lib/shared/viewmodels/user_viewmodel.dart
@@ -4,9 +4,9 @@ import 'package:geolocator/geolocator.dart';
 import 'package:sos/shared/models/user.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:sos/shared/repositories/user_repository.dart';
-import 'package:sos/shared/utils/log_util.dart';
 
 // JWT token을 이용하여 payload를 decode하여 사용자 정보를 가져오는 방식으로 변경
+// -> UserDetail API를 호출하여 정보를 가져오는 방식으로 변경
 class UserViewModel extends StateNotifier<User> {
   final FlutterSecureStorage secureStorage = FlutterSecureStorage();
   final UserRepository userRepository = UserRepository();
@@ -18,28 +18,37 @@ class UserViewModel extends StateNotifier<User> {
           profilePicture: null,
           email: null,
           nickname: null,
-        ));
-
-  Future<User> loadUserInfo() async {
-    final accessToken = await secureStorage.read(key: 'access_token');
-    if (accessToken != null) {
-      Map<String, dynamic> decodedToken = JwtDecoder.decode(accessToken);
-
-      final user = User(
-        name: decodedToken['name'],
-        profilePicture: decodedToken['profilePicture'],
-        email: decodedToken['email'],
-        nickname: decodedToken['nickname'],
-      );
-
-      state = user;
-
-      return user;
-    } else {
-      LogUtil.e('No access token found');
-      return User(name: '사용자를 찾을 수 없습니다.');
-    }
+        )) {
+    _initialize();
   }
+
+  Future<void> _initialize() async {
+    await loadUserInfo();
+  }
+
+  Future<void> loadUserInfo() async {
+    final user = await userRepository.getUserDetail();
+    state = user;
+  }
+
+  // Future<User> loadUserInfoFromToken() async {
+  //   final accessToken = await secureStorage.read(key: 'access_token');
+  //   if (accessToken != null) {
+  //     Map<String, dynamic> decodedToken = JwtDecoder.decode(accessToken);
+
+  //     final user = User(
+  //       name: decodedToken['name'],
+  //       profilePicture: decodedToken['profilePicture'],
+  //       email: decodedToken['email'],
+  //       nickname: decodedToken['nickname'],
+  //     );
+  //     state = user;
+  //     return user;
+  //   } else {
+  //     LogUtil.e('No access token found');
+  //     return User(name: '사용자를 찾을 수 없습니다.');
+  //   }
+  // }
 
   Future<void> updateUserLocation() async {
     final position = await Geolocator.getCurrentPosition(
