75e3c9a740b67eb685b5514892ab05869fc1d4fc
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-10-01 05:04:15 +0900

    [Fix] refactor map widget

--- lib/features/home/views/widgets/map_area.dart
+++ lib/features/home/views/widgets/map_area.dart
@@ -43,25 +43,7 @@ class MapWidget extends ConsumerWidget {
             ),
           ),
           onMapReady: (NaverMapController controller) async {
-            // 지도 컨트롤러 초기화
-            ref
-                .read(mapControllerProvider.notifier)
-                .initializeController(controller);
-            // NLocationTrackingMode 설정 (face, follow, noFollow, none)
-            controller.setLocationTrackingMode(NLocationTrackingMode.face);
-            controller
-                .getLocationOverlay()
-                .setCircleColor(AppColors.lightBlue.withOpacity(0.3));
-
-            // 지도에 post 마커 추가
-            await mapViewModel.fetchPostsForMap(
-                level, currentLocation.latitude, currentLocation.longitude, 15);
-
-            _addMarkers(controller, context, ref);
-
-            // 친구 마커 추가
-            await friendViewModel.fetchFriendsLocation();
-            _addFriendMarkers(controller, context, ref);
+            await _initializeMap(controller, ref, context);
           },
           onCameraIdle: () async {
             final naverMapController = ref.read(mapControllerProvider);
@@ -69,19 +51,14 @@ class MapWidget extends ConsumerWidget {
               final cameraPosition =
                   await naverMapController.getCameraPosition();
               final zoomLevel = cameraPosition.zoom.round();
-              await mapViewModel.fetchPostsForMap(
-                  level,
-                  cameraPosition.target.latitude,
-                  cameraPosition.target.longitude,
-                  zoomLevel);
-              // bottomSheetViewModel.fetchNearbyEvents(
-              //     cameraPosition.target.latitude,
-              //     cameraPosition.target.longitude);
-              _addMarkers(naverMapController, context, ref);
 
-              // 친구 마커 추가
-              await friendViewModel.fetchFriendsLocation();
-              _addFriendMarkers(naverMapController, context, ref);
+              await mapViewModel.fetchPostsForMap(
+                level,
+                cameraPosition.target.latitude,
+                cameraPosition.target.longitude,
+                zoomLevel,
+              );
+              await _updateMarkers(naverMapController, ref, context);
             }
           },
           onMapTapped: (point, latLng) {
@@ -104,6 +81,34 @@ class MapWidget extends ConsumerWidget {
     );
   }
 
+  Future<void> _initializeMap(
+    NaverMapController controller,
+    WidgetRef ref,
+    BuildContext context,
+  ) async {
+    // 지도 컨트롤러 초기화
+    ref.read(mapControllerProvider.notifier).initializeController(controller);
+
+    // NLocationTrackingMode 설정 (face, follow, noFollow, none)
+    controller.setLocationTrackingMode(NLocationTrackingMode.face);
+    controller
+        .getLocationOverlay()
+        .setCircleColor(AppColors.lightBlue.withOpacity(0.3));
+
+    // 지도에 post 마커 및 친구 마커 추가
+    await _updateMarkers(controller, ref, context);
+  }
+
+  Future<void> _updateMarkers(
+    NaverMapController controller,
+    WidgetRef ref,
+    BuildContext context,
+  ) async {
+    // 지도 마커와 친구 마커를 업데이트
+    await _addMarkers(controller, context, ref);
+    await _addFriendMarkers(controller, context, ref);
+  }
+
   final Set<String> currentMarkerIds = {};
 
   Future<void> _addMarkers(
@@ -114,76 +119,88 @@ class MapWidget extends ConsumerWidget {
     final mapViewModel = ref.watch(mapViewModelProvider);
     final posts = mapViewModel;
 
-    //controller.clearOverlays();
-
     final Set<String> newMarkerIds =
         posts.map((post) => post.postId.toString()).toSet();
 
     for (var post in posts) {
       final markerId = post.postId.toString();
-
       if (!currentMarkerIds.contains(markerId)) {
-        // Add new markers
-        NMarker marker;
-        if (post.mediaURL != null && post.mediaURL!.isNotEmpty) {
-          final nOverlayImage = await buildImageMarkerWidget(
-            post.mediaURL!,
-            post.disasterType!,
-            context,
-          );
-          marker = CustomImageMarker(
-            id: markerId,
-            nOverlayImage: nOverlayImage,
-            position: NLatLng(post.latitude!, post.longitude!),
-            onTap: () {
-              ref
-                  .read(mapViewModelProvider.notifier)
-                  .onMarkerTap(post, ref, controller);
-            },
-          );
-        } else {
-          marker = CustomMarker(
-            id: markerId,
-            eventType: post.disasterType!,
-            position: NLatLng(post.latitude!, post.longitude!),
-            onTap: () {
-              ref
-                  .read(mapViewModelProvider.notifier)
-                  .onMarkerTap(post, ref, controller);
-            },
-          );
-        }
-
+        final marker = await _buildMarker(post, context, ref, controller);
         controller.addOverlay(marker);
-        currentMarkerIds.add(markerId); // Track the added marker
+        currentMarkerIds.add(markerId);
       }
     }
 
-    // Remove old markers
-    // final markersToRemove = currentMarkerIds.difference(newMarkerIds);
-    // for (final markerId in markersToRemove) {
-    //   await controller.deleteOverlay(
-    //     NOverlayInfo(
-    //       type: NOverlayType.marker,
-    //       id: markerId,
-    //     ),
-    //   );
-    //   currentMarkerIds.remove(markerId); // Remove from current tracking
-    // }
+    // 기존 마커 중에서 더 이상 존재하지 않는 마커 제거
+    // TODO: 오류 해결 (_AssertionError ('package:flutter_naver_map/src/controller/overlay/overlay_controller_impl.dart': Failed assertion: line 49 pos 12: 'helper != null': Not Added or Already Deleted this overlay : NOverlayInfo(type: NOverlayType.marker, id: 1)))
+    //_removeOldMarkers(controller, newMarkerIds);
+  }
+
+  Future<NMarker> _buildMarker(
+    post,
+    BuildContext context,
+    WidgetRef ref,
+    NaverMapController controller,
+  ) async {
+    if (post.mediaURL != null && post.mediaURL!.isNotEmpty) {
+      final nOverlayImage = await buildImageMarkerWidget(
+        post.mediaURL!,
+        post.disasterType!,
+        context,
+      );
+      return CustomImageMarker(
+        id: post.postId.toString(),
+        nOverlayImage: nOverlayImage,
+        position: NLatLng(post.latitude!, post.longitude!),
+        onTap: () {
+          ref
+              .read(mapViewModelProvider.notifier)
+              .onMarkerTap(post, ref, controller);
+        },
+      );
+    } else {
+      return CustomMarker(
+        id: post.postId.toString(),
+        eventType: post.disasterType!,
+        position: NLatLng(post.latitude!, post.longitude!),
+        onTap: () {
+          ref
+              .read(mapViewModelProvider.notifier)
+              .onMarkerTap(post, ref, controller);
+        },
+      );
+    }
+  }
+
+  void _removeOldMarkers(
+      NaverMapController controller, Set<String> newMarkerIds) {
+    final markersToRemove = currentMarkerIds.difference(newMarkerIds);
+
+    for (final markerId in markersToRemove) {
+      if (currentMarkerIds.contains(markerId)) {
+        try {
+          // 마커가 존재하는 경우에만 삭제
+          controller.deleteOverlay(
+            NOverlayInfo(type: NOverlayType.marker, id: markerId),
+          );
+          currentMarkerIds.remove(markerId); // 삭제 후 트래킹 리스트에서도 제거
+        } catch (e) {
+          // 예외가 발생하면 에러 처리
+          debugPrint("Failed to remove marker with id: $markerId, error: $e");
+        }
+      }
+    }
   }
 
   Future<void> _addFriendMarkers(
-    //친구 마커 추가
     NaverMapController controller,
     BuildContext context,
     WidgetRef ref,
   ) async {
-    final friendViewModel = ref.watch(friendViewModelProvider);
-    final friends = friendViewModel;
+    final friends = ref.watch(friendViewModelProvider);
 
     for (var friend in friends) {
       final markerId = friend.favoriteMemberId.toString();
-
       if (!currentMarkerIds.contains(markerId)) {
         final marker = FriendMarker(
           id: markerId,
@@ -195,24 +212,9 @@ class MapWidget extends ConsumerWidget {
                 .onFriendMarkerTap(friend, ref, controller);
           },
         );
-
         controller.addOverlay(marker);
-        currentMarkerIds.add(markerId); // Track the added marker
+        currentMarkerIds.add(markerId);
       }
-    }
-  }
-
-  Future<void> _clearFriendMarkers(NaverMapController controller,
-      BuildContext context, WidgetRef ref) async {
-    final friendViewModel = ref.watch(friendViewModelProvider);
-    final friends = friendViewModel;
-    for (var friend in friends) {
-      await controller.deleteOverlay(
-        NOverlayInfo(
-          type: NOverlayType.marker,
-          id: 'friend' + friend.favoriteMemberId.toString(),
-        ),
-      );
     }
   }
 }
