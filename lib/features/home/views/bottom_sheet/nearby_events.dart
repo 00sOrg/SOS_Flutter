594880363643bbce9cdc5467a036d6ea7a43952a
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-10-01 23:01:08 +0900

    [Feat] Map search

--- lib/features/home/views/bottom_sheet/nearby_events.dart
+++ lib/features/home/views/bottom_sheet/nearby_events.dart
@@ -111,10 +111,11 @@ class NearbyEvents extends ConsumerWidget {
                     ref
                         .read(bottomSheetViewModelProvider.notifier)
                         .navigateToPost(context, event.postId),
-                    ref.read(mapViewModelProvider.notifier).centerMapOnPost(
-                          //TODO: api 수정완료되면 고쳐야함
-                          event.latitude ?? 37.22,
-                          event.longitude ?? 127.04,
+                    ref
+                        .read(mapViewModelProvider.notifier)
+                        .updateCameraToCenter(
+                          event.latitude!,
+                          event.longitude!,
                           naverMapController!,
                         ),
                   },
--- lib/shared/services/location_service.dart
+++ lib/shared/services/location_service.dart
@@ -31,4 +31,33 @@ class LocationService {
 
     return jsonData;
   }
+
+  // 주소를 좌표로 변환하는 검색 메소드 추가
+  Future<Map<String, dynamic>> searchLocationByAddress(String address) async {
+    final String baseGeocodeUrl = dotenv.env['NAVER_GEOCODE_URL']!;
+    final String fullUrl =
+        '$baseGeocodeUrl?query=${Uri.encodeComponent(address)}&output=json';
+
+    final headers = {
+      'X-NCP-APIGW-API-KEY-ID': dotenv.env['NAVER_MAP_API_ID']!,
+      'X-NCP-APIGW-API-KEY': dotenv.env['NAVER_MAP_API_SECRET']!,
+    };
+
+    final response = await http.get(
+      Uri.parse(fullUrl),
+      headers: headers,
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception("Failed to search location: ${response.reasonPhrase}");
+    }
+
+    final jsonData = jsonDecode(response.body);
+
+    if (jsonData["addresses"] == null || jsonData["addresses"].isEmpty) {
+      throw RangeError("No results found for the given address");
+    }
+
+    return jsonData;
+  }
 }
